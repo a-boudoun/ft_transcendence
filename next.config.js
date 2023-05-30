da7fd319631bdab766f9971c17bf0bd613b64b10
Abderrazzak Miski <amiski@e1r6p12.1337.ma>
2023-05-30 16:48:03 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ['cdn.intra.42.fr'],
+      },
+    }
