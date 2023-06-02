dafa17e0d9d23c6c0ae2a285781aad929373ab81
Abderrazzak1m <abderrazzakmiski8@gmail.com>
2023-06-02 16:20:30 +0100

    remove

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-module.exports = {
-    images: {
-        domains: ['cdn.intra.42.fr'],
-      },
-    }
