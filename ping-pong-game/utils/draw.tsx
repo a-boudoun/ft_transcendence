0a27833754cd2ae46a2893a658fcb116a2fcc980
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-06-06 12:29:36 +0100

    adding utils

---
+++ ping-pong-game/utils/draw.tsx
@@ -0,0 +1,27 @@
+
+import { Bodies } from 'matter-js'
+
+export function drawRect(x : number, y : number, w : number, h : number, color : string) {
+	return (
+		Bodies.rectangle(x, y, w, h, {
+			isStatic: true,
+			render: {
+				fillStyle: color,
+			},
+		})
+	)
+}
+
+export function drawCircle(x : number, y : number, r : number, color : string) {
+	return (
+		Bodies.circle(x, y, r, {
+			restitution: 1, // Make the ball fully elastic
+    		friction: 0, // Remove friction
+    		frictionAir: 0, // Remove air friction
+			inertia: Infinity, // prevent ball from slowing down
+			render: {
+				fillStyle: color,
+			},
+		})
+	)
+}
