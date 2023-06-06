5801e2b8a62003855451051dcf0a8eb06bd2a1c7
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-06-06 21:45:14 +0100

    trying to move the rectangular

--- ping-pong-game/app/page.tsx
+++ ping-pong-game/app/page.tsx
@@ -4,25 +4,25 @@ import React, { useEffect, useRef, useState } from "react";
 import {Engine, Render, World, Body} from "matter-js";
 import { drawRect, drawCircle } from "@/utils/draw";
 
-
 export default function Game(){
 	// let board: number = 300;
 	const canvasRef = useRef(null);
 	const timeToStart: number = 3;
 	const [countDownValue, setCountDownValue] = useState<number>(timeToStart);
 	const [pVisible, setPVisible] = useState<boolean>(true);
-	const  [board, setBoard] = useState<number>(300);
+	const  [boardY, setBoard] = useState<number>(300);
+	const  [renderRef, setRenderRef] = useState<Render>();
 	
 	// to prevent infinite loop
 	const handleKeyDown = (event) => {
 		console.log(event.key);
 		if (event.key === "ArrowUp"){
-			if (board > 0){
-				setBoard(board - 10);
+			if (boardY > 0){
+				setBoard(boardY - 10);
 			}
 		}else if (event.key === "ArrowDown"){
-			if (board < 450){
-				setBoard(board + 10);
+			if (boardY < 450){
+				setBoard(boardY + 10);
 			}
 		}
 	}
@@ -35,7 +35,6 @@ export default function Game(){
 		}
 	}, [countDownValue]);
 	
-
 	useEffect(() => {
 	
 		document.addEventListener("keydown", handleKeyDown);
@@ -48,36 +47,37 @@ export default function Game(){
 				scale:0.001,
 			},
 		}),
-			render = Render.create({
-				engine: engine,
-				canvas: canvasRef.current,
-				options: {
-					width: 800,
-					height: 600,
-					// rendering pixel to be more sharp
-					pixelRatio: 2,
-					wireframes: false,
-					background: "#7AC7C4",
-				}
-			});
+		render = Render.create({
+			engine: engine,
+			canvas: canvasRef.current,
+			options: {
+				width: 800,
+				height: 600,
+				// rendering pixel to be more sharp
+				pixelRatio: 2,
+				wireframes: false,
+				background: "#7AC7C4",
+			}
+		});
 
 		const floor = drawRect(400, 600, 800, 20, '#92a7ad');
-		const rightBoard = drawRect(770, board, 20, 150, '#EA5581');
+		const rightBoard = drawRect(770, 300, 20, 150, '#EA5581');
 		const ceiling = drawRect(400, 0, 800, 20, '#92a7ad');
 		const leftBoard = drawRect(30, 300, 20, 150, '#EA5581');
 		const leftWall = drawRect(10, 300, 15, 560, '#91adcc');
 		const rightWall = drawRect(790, 300, 15, 560, '#91adcc');
 		
 		const ball = drawCircle(400, 200, 15, '#384259');
-		// Body.setVelocity(ball, { x: 10, y: 3 }); // Set the ball moving speed
-		const start =  () => Body.setVelocity(ball, { x: 10, y: 3 }); // Set the ball moving speed
+		const start =  () => Body.setVelocity(ball, { x: 8, y: 3 }); // Set the ball moving speed
 		setTimeout(start, timeToStart * 1000);
+		setPVisible(true);
+		setCountDownValue(timeToStart);
 		
 		World.add(engine.world, [floor, ball, ceiling, rightBoard, leftBoard, leftWall, rightWall]);
 		
 		Engine.run(engine);
 		Render.run(render);
-
+		setRenderRef(render);
 		// Remove event listener when component unmounts
 		return () => {
 		  document.removeEventListener("keydown", handleKeyDown);
@@ -87,6 +87,7 @@ export default function Game(){
 	return (
 		<div className="flex justify-center relative mt-9">
 			<canvas ref={canvasRef}/>
+			<Rightb renderRef={renderRef} value={boardY}/>
 			{pVisible && <p className="absolute mt-4 font-bold text-white text-[50px]">{countDownValue}</p>}
 		</div>
 	);
