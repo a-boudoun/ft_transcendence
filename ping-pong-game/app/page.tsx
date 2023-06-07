2b69b39e1cf5bc12c876398042aee0ed8a348455
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-06-07 17:32:23 +0100

    keydown working

--- ping-pong-game/app/page.tsx
+++ ping-pong-game/app/page.tsx
@@ -1,34 +1,32 @@
 "use client";
 
-import React, { use, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {Engine, Render, World, Body} from "matter-js";
 import { drawRect, drawCircle } from "@/utils/draw";
-import { redirect } from "next/dist/server/api-utils";
 
 export default function Game(){
-	// let board: number = 300;
+	let board: number = 300;
+	const moveSpeed: number = 10;
 	const canvasRef = useRef(null);
 	const timeToStart: number = 3;
 	const [countDownValue, setCountDownValue] = useState<number>(timeToStart);
 	const [pVisible, setPVisible] = useState<boolean>(true);
-	const  [boardY, setBoard] = useState<number>(300);
+	const  [rightBoardY, setRightBoard] = useState<number>(300);
 	const  [renderRef, setRenderRef] = useState<Render>();
 	
 	// to prevent infinite loop
 	const handleKeyDown = (event: any) => {
-		console.log("boardY keyhandle before: ", boardY);
+		// TODO: work with boardY instead of board
 		if (event.key === "ArrowUp"){
-			if (boardY > 0){
-				console.log("boardY UP if: ", boardY);
-				setBoard(boardY - 10);
+			if (board - moveSpeed > 0){
+				board -= moveSpeed;
 			}
 		}else if (event.key === "ArrowDown"){
-			if (boardY < 450){
-				console.log("boardY DOWN if: ", boardY);
-				setBoard(boardY + 10);
+			if (board + moveSpeed < 500){
+				board += moveSpeed;
 			}
 		}
-		console.log("boardY keyhandle after: ", boardY);
+		setRightBoard(board);
 	}
 
 	useEffect(() => {
@@ -65,7 +63,6 @@ export default function Game(){
 			}
 		});
 		
-		console.log("rerender");
 		const floor = drawRect(400, 600, 800, 20, '#92a7ad');
 		const rightBoard = drawRect(770, 300, 20, 150, '#EA5581');
 		const ceiling = drawRect(400, 0, 800, 20, '#92a7ad');
@@ -92,10 +89,9 @@ export default function Game(){
 
 		useEffect(() => {
 			if (!renderRef) return;
-			console.log("boardY change: ", boardY);
-			Body.setPosition(renderRef.engine.world.bodies[3], {x: 770, y: boardY});
+			Body.setPosition(renderRef.engine.world.bodies[3], {x: 770, y: rightBoardY});
 
-		} , [boardY]);
+		} , [rightBoardY]);
 
 	return (
 		<div className="flex justify-center relative mt-9">
