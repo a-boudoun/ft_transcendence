327302a382936cc991b11cc002a50d802276aa0f
Mohamed Azhari <mazhari@e1r8p15.1337.ma>
2023-06-09 20:31:53 +0100

    finish Oauth  42  api and Jwt authecetion with a static arry for testing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(cookieParser());
+  app.enableCors();
    
   await app.listen(8000);
 }
