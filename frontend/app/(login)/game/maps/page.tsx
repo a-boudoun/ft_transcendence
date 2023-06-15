9b606ff5231a780d84cd217ff52d7a15b2c9a1be
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-06-15 18:59:23 +0100

    replacing maps icons

--- frontend/app/(login)/game/maps/page.tsx
+++ frontend/app/(login)/game/maps/page.tsx
@@ -8,9 +8,9 @@ export default function Maps(){
 		<div className="h-screen p-[56px] flex justify-center  bg-dark-gray">
 			<section className="bg-[#4B5468] w-[200px] h-[60px] flex items-center justify-center rounded-[10px] mt-[150px] text-[#B5B3BD]">selet the map</section>
 			<div className="bg-red h-[300px] w-[300px] flex justify-center mt-[300px]">
-				map1
-				map2
-				map3
+				<img src="/game/m3a-m3a.svg"/>
+				<img src="/game/ched-ched.svg"/>
+				<img src="/game/default-map.svg"/>
 			</div>
 			<button className="absolute bottom-[40px] right-[40px] m-4  text-white text-[20px] bg-red w-[150px] h-[40px] rounded-[10px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/table')}}>
 				play
