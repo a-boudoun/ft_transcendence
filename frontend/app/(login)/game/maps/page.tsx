dc8cf2323cfa7c6e82d76b1001c54d057b39dcfe
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-06-17 15:58:03 +0100

    adding boarder

--- frontend/app/(login)/game/maps/page.tsx
+++ frontend/app/(login)/game/maps/page.tsx
@@ -10,21 +10,24 @@ export default function Maps(){
 		<div className="h-screen w-full bg-dark-gray mt-[56px] py-4 pt-[200px]">
 			<div className="w-full px-2">
 				<div 
-					className="bg-[#4B5468] m-auto w-[200px] my-4 py-4 rounded-[10px] text-[#B5B3BD] text-center"
+					className="bg-[#4B5468] m-auto mb-[70px] w-[300px]  py-4 rounded-[10px] text-[#B5B3BD] font-medium text-center"
 				>
 					selet the map
 				</div>
 				
 				<div className="flex justify-center gap-8 items-center flex-col sm:flex-row w-full">
 
-					<div className="w-full max-w-[320px]">
+					<div className="w-full max-w-[320px] hover:border-2 hover:border-[#B5B3BD] hover:rounded-[10px]">
 						<Image width={100} height={100} alt="#" src="/game/m3a-m3a.svg" className="h-full w-full"/>
+						<p className="text-white text-center mt-[10px] font-bold">m3a-m3a</p>
 					</div>
-					<div className="w-full max-w-[320px] ">
+					<div className="w-full max-w-[320px] hover:border-2 hover:border-[#B5B3BD] hover:rounded-[10px]">
 						<Image width={100} height={100} alt="#" src="/game/ched-ched.svg" className="h-full w-full"/>
+						<p className="text-white text-center mt-[10px] font-bold">ched-ched</p>
 					</div>
-					<div className="w-full max-w-[320px] ">
+					<div className="w-full max-w-[320px] hover:border-2 hover:border-[#B5B3BD] hover:rounded-[10px]">
 						<Image width={100} height={100} alt="#" src="/game/default-map.svg" className="h-full w-full"/>
+						<p className="text-white text-center mt-[10px] font-bold border">default</p>
 					</div>
 
 				</div>
