ad233104bc6fc773763e73ca93eb7c3173172125
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-06-21 19:09:50 +0100

    resizing buttons

--- frontend/app/(login)/game/page.tsx
+++ frontend/app/(login)/game/page.tsx
@@ -7,13 +7,13 @@ export default function Gamehome(){
 	return (
 		<div className="h-screen p-[56px] flex items-center justify-center  bg-dark-gray">
 			<div className="h-[800px] w-[800px] flex flex-col items-center justify-center space-y-20 bg-[#4D566A] rounded-[30px]"> 
-  				<button className="text-white text-[40px] bg-red w-[400px] h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/maps')}}>
+  				<button className="text-white text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/maps')}}>
    				 online game
   				</button>
-  				<button className="text-white text-[40px] bg-red w-[400px] h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/table')}}>
+  				<button className="text-white text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/table')}}>
    				 offline game
   				</button>
-  				<button className="text-white text-[40px] bg-red w-[400px] h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/table')}}>
+  				<button className="text-white text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/table')}}>
    				 play with AI
   				</button>
 			</div>
