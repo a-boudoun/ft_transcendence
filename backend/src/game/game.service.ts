fb8a7ace11b49ed47658373c4bc6f65892cf9e52
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-07-11 18:52:54 +0100

    working on backend

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -3,5 +3,9 @@ import { Player } from "./interfaces/player.interface";
 
 @Injectable()
 export class gameService{
-
+	private readonly players: Player[] = [];
+	createPlayer(player: Player){
+		player.id =  
+		this.players.push(player);
+	}
 }
