dca16ef962cc3451fe16c80c57cc59c6210814c6
Mohamed Azhari <mazhari@e3r7p9.1337.ma>
2023-07-22 19:32:42 +0100

    working on settings

--- frontend/app/(login)/settings/page.tsx
+++ frontend/app/(login)/settings/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import UpdateForm from '@/components/settings/UpdateForm'
 
 const Settings = () => {
   return (
     <>
-    <main className="h-screen p-[56px] bg-dark-gray">
-      <UpdateForm />
+    <main className="h-full p-[56px] bg-dark-gray sm:pt-[96px]">
+      <div className='h-full max-w-[640px] grow flex flex-col sm:mx-auto sm:bg-white sm:bg-opacity-20 sm:ackdrop-blur-lg sm:drop-shadow-lg sm:p-4 sm:rounded-[2.5rem] sm:shadow-2xl'>
+        <UpdateForm />
+      </div>
     </main>
     </>
   )
