856d2e990c807fa66e4d9438119b45a3420b51cd
Mohamed Azhari <mazhari@e3r7p9.1337.ma>
2023-07-22 15:51:11 +0100

    adding loader animation when signIn

--- frontend/app/(login)/settings/page.tsx
+++ frontend/app/(login)/settings/page.tsx
@@ -4,7 +4,7 @@ const Settings = () => {
   return (
     <>
     <main className="h-screen p-[56px] bg-dark-gray">
-      <h1 className='text-white'>Settings</h1>   
+      <UpdateForm />
     </main>
     </>
   )
