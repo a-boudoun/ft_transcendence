460810e5defc493d4a3ff52a3932f6c133139c21
Mohamed Azhari <mazhari@e3r2p16.1337.ma>
2023-07-25 20:34:35 +0100

    letting the user to change his name and image from settings

--- frontend/app/(login)/settings/page.tsx
+++ frontend/app/(login)/settings/page.tsx
@@ -5,7 +5,7 @@ const Settings = () => {
   return (
     <>
     <main className="h-full p-[56px] bg-dark-gray sm:pt-[96px]">
-      <div className='h-full max-w-[640px] grow flex flex-col sm:mx-auto sm:bg-white sm:bg-opacity-20 sm:ackdrop-blur-lg sm:drop-shadow-lg sm:p-4 sm:rounded-[2.5rem] sm:shadow-2xl'>
+      <div className='h-full max-w-[640px] grow flex flex-col sm:mx-auto sm:bg-white sm:bg-opacity-20 sm:ackdrop-blur-lg sm:drop-shadow-lg sm:rounded-[2.5rem] sm:shadow-2xl'>
         <UpdateForm />
       </div>
     </main>
--- frontend/components/settings/UpdateForm.tsx
+++ frontend/components/settings/UpdateForm.tsx
@@ -1,13 +1,22 @@
 'use client'
 
-import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react';
 import Image from 'next/image';
-import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { Client  } from '@/Providers/QueryProvider';
+import { useQuery , useMutation } from "@tanstack/react-query";
+import { Loader2 } from  'lucide-react';
+import { userSchema } from '@/models/user';
+import { userDto } from '@/dto/userDto';
+import uploadImage from '@/apis/uploadImage';
 
 const UpdateForm = () => {
+
+  const [image, setImage] = useState<any>(null);
+  const [imagePreview, setImagePreview] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [errors, setErrors] = useState<z.ZodIssue[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const User = useQuery({
     queryKey: ['user'],
@@ -16,26 +25,80 @@ const UpdateForm = () => {
       return data;
     }
   });
-  
-  const [image, setImage] = useState<any>(null);
-  const [imagePreview, setImagePreview] = useState<string>(user.image);
-  const [name, setName] = useState<string>(user.name);
 
-    return (
-        <>
-        <form className='flex items-center gap-8'>
-          <label>
-            <div className='relative hover:opacity-60'>
-                <Image className='w-[200px] h-[200px] rounded-full cursor-pointer' src={imagePreview} width={1000} height={1000} alt="avatar" />
-                <Image className='absolute bottom-5 right-0' src={"/icons/changeImage.svg"} width={32} height={32} alt="" />
-                <input type="file" className="hidden" accept="image/jpeg, image/jpg, image/png, image/webp" />
+  const updateUser = useMutation({
+    mutationFn: async(user : userDto) => {
+      await axios.patch('http://localhost:8000/users/updateMe', user, { withCredentials: true });
+    },
+    onSuccess: () => {
+      console.log('success');
+      setIsLoading(false);
+    },
+  });
+
+  useEffect(() => {
+    if (!User.isLoading) {
+      setName(User.data.name);
+      setImagePreview(User.data.image);
+    }
+  }, [User.isLoading, User.data]);
+
+  const handleChange = async (e: any) => {
+    if (e.target.files){
+      setImage(e.target.files[0]);
+      setImagePreview(URL.createObjectURL(e.target.files[0]));
+    }
+    else if (e.target.value.length > 0)
+      setName(e.target.value);
+  };
+
+  const handleSubmit = async(e: any) => {
+    e.preventDefault(); 
+
+    const validationResult = await userSchema.safeParseAsync({name: name, image: image});
+    if (validationResult.success) {
+      User.data.name = name;
+      if (image)
+      {
+        const uploadimage = await uploadImage(image);
+        User.data.image = uploadimage;
+      }
+      await updateUser.mutate(User.data);
+    }
+    else
+    {
+      setIsLoading(false);
+      setErrors(validationResult.error.issues);
+    }
+  };
+
+
+  if (User.isLoading)
+    return <div>loading...</div>
+  else {
+      return (
+          <>
+          <form className='p-[56px]'  onChange={handleChange} onSubmit={handleSubmit}>
+            <h3 className='mb-6 text-xl font-bold text-blue' >Account settings</h3>
+            <div className='flex justify-center items-center gap-8'>
+              <label>
+                <div className='relative hover:opacity-60'>
+                    <Image className='min-w-[132px] min-h-[132px] sm:w-[200px] sm:h-[200px] rounded-full cursor-pointer' src={imagePreview} width={1000} height={1000} alt="avatar" />
+                    <Image className='absolute bottom-5 right-0 sm:w-8 sm:h-8' src={"/icons/changeImage.svg"} width={24} height={24} alt="" />
+                    <input type="file" className="hidden" accept="image/jpeg, image/jpg, image/png, image/webp" />
+                </div>
+              </label>
+              <input id={'name'} className="h-16 rounded-2xl text-black text-center focus:outline-0 focus:border-black focus:border-[2px] hover:opacity-60" type="text" placeholder={name}/>
             </div>
-          </label>
-          <input id={'name'} className="h-16 rounded-2xl text-black text-center focus:outline-0 focus:border-black focus:border-[2px] hover:opacity-60" type="text" placeholder={user.name}/>
-        </form>
-        {/* {errors.length > 0 && <p className='text-red text-center max-w-[200px]'>{errors[0].message}</p>} */}
-        </>
-  )
+            <h3 className='my-6 text-xl font-bold text-blue' >two-factor authentication</h3>
+            <button className="relative mt-12 h-16 rounded-2xl text-black text-center bg-blue px-14 hover:opacity-60" type='submit' onClick={ () => setIsLoading(true)} >save
+              {isLoading && <Loader2 className="absolute top-6 right-6 animate-spin" size={20} strokeWidth={1.2} />}
+            </button>
+          </form>
+          {/* {errors.length > 0 && <p className='text-red text-center max-w-[200px]'>{errors[0].message}</p>} */}
+          </>
+    )
+  }
 }
 
 export default UpdateForm;
