0de21f95ab036da527b8c37d01e1d28aaea73fd5
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-07-26 17:58:07 +0100

    adding game simulation for every room

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { gameService } from "./game.service";
 import { GameGateway } from "./game.gateway";
 import { engineService } from "./engine.service";
+import { gameSimulation } from "./gameSimulation.service";
 
 @Module({
-	providers: [gameService, GameGateway, engineService],
+	providers: [gameService, GameGateway, engineService, gameSimulation],
 	exports: [gameService, GameGateway, engineService]
 })
 export class GameModule{}
