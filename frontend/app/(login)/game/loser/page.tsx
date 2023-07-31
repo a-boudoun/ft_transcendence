36e261b52778f86e25fad80b1afa39c7ac706dfd
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-07-31 17:32:02 +0100

    adding lost and won pages

--- frontend/app/(login)/game/loser/page.tsx
+++ frontend/app/(login)/game/loser/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Image from "next/image";
 
-export default function Won(){
+export default function Lost(){
   return (
-    <div className='bg-red'>
-      <h1>You Won!</h1>
-	  <Image width={100} height={100} alt="#" src="/game/m3a-m3a.svg" className="h-full w-full"/>
-	  {/* https://tenor.com/view/lord-gif-21099221 */}
+    <div>
+	  <Image width={100} height={100} alt="#" src="/game/game-lost.svg" className="h-full w-full"/>
     </div>
   );
 };
--- frontend/app/(login)/game/winner/page.tsx
+++ frontend/app/(login)/game/winner/page.tsx
@@ -3,10 +3,8 @@ import Image from "next/image";
 
 export default function Won(){
   return (
-    <div className='bg-red'>
-      <h1>You Won!</h1>
-	  <Image width={100} height={100} alt="#" src="/game/m3a-m3a.svg" className="h-full w-full"/>
-	  {/* https://tenor.com/view/lord-gif-21099221 */}
+    <div>
+	  <Image width={100} height={100} alt="#" src="/game/game-win.svg" className="h-full w-full"/>
     </div>
   );
 };
