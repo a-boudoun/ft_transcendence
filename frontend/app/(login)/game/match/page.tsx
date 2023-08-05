b6e8ceb9082add762a66a3019bde5ec8be897390
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-08-05 20:10:07 +0100

    handling refrech on matchmaking

--- frontend/app/(login)/game/match/page.tsx
+++ frontend/app/(login)/game/match/page.tsx
@@ -1,16 +1,20 @@
 'use client';
 
-import React from 'react';
+import React, { use } from 'react';
 import { useEffect, useState } from 'react';
 import socket from '@/components/socketG';
 import Image from "next/image";
 import axios from 'axios';
 import { useQuery } from "@tanstack/react-query";
+import { set } from 'zod';
 
 interface prop {
- clicked?: boolean;
- setClicked: (val: boolean) => void;
+ clicked?: number;
+ setClicked: (val: number) => void;
 }
+
+// TODO: remove global data
+var globalData : any;
 
 function LeftPlayer(){
 
@@ -18,6 +22,7 @@ function LeftPlayer(){
 		queryKey: ['user'],
 		queryFn: async ()=> {
 		  const {data} = await axios.get('http://localhost:8000/users/me', { withCredentials: true })
+		  globalData = data;
 		  return data;
 		}
 	  });
@@ -55,6 +60,7 @@ function LoadingPlayer({setClicked}: prop){
 		"/game/woman2.png",
 		"/game/woman3.png",
 	];
+
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setImage(iamges[Math.floor(Math.random() * iamges.length)]);
@@ -68,30 +74,31 @@ function LoadingPlayer({setClicked}: prop){
 				  return data;
 				}
 			  });
-			if (data) {
-				setImage(data.image);
-				setName(data.username);
-			}
-		});
-		return () => {
-			clearInterval(interval)
-		};
-	}, []);
-
-
-	return (
-		<>
+			  if (data) {
+				  setClicked(3);
+				  setImage(data.image);
+				  setName(data.username);
+				}
+			});
+			return () => {
+				clearInterval(interval)
+			};
+		}, []);
+		
+		
+		return (
+			<>
 			 <Image src={image} width={200} height={200} alt="avatar" className="w-full h-full rounded-full"/>
 			 <h1 className = 'pt-2 text-2xl font-thin '> {name} </h1>
 		</>
 	)
-
+	
 }
 
 function RightPlayer({ clicked, setClicked } : prop){
-
+	
 	useEffect(() => {
-
+		
 	}, []);
 	return (
 		<div className = 'flex flex-col iterms-center justify-center  w-[150px] h-[150px]'>
@@ -101,8 +108,14 @@ function RightPlayer({ clicked, setClicked } : prop){
 }
 
 export default function MatchPlayers(){
-	const [look, setLook] = useState<boolean>(false);
+	const [look, setLook] = useState<number>(0);
 
+		useEffect(() => {
+		socket.emit('already-looking', globalData.username);
+		socket.on('already-looking', () => {
+			setLook(1);
+		});
+	}, []);
 	return (
 		<div className = 'flex flex-col items-center justify-center w-screen h-screen bg-dark-gray gap-2'>
 			<main className = 'flex flex-row bg-[#384259] w-[500px] h-[300px] items-center justify-between px-8 rounded-[10px]'>
@@ -110,22 +123,25 @@ export default function MatchPlayers(){
 				<h1 className = 'text-white text-5xl font-serif'>VS</h1>
 				<RightPlayer clicked={look} setClicked={setLook}/>
 			</main>
+			{ look !== 3 && <div>
+				{ look === 0 && <button 
+				className=" max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-red w-[250px] py-2 rounded-[10px] hover:bg-[#FBACB3] font-bold"
+				onClick = {() => {
+					socket.emit('looking-for-match', globalData.username);
+					setLook(1);
+				}}>
+				find a player
+				</button>}
 
-			{ !look && <button 
-			className=" w-full max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-red w-[150px] py-2 rounded-[10px] hover:bg-[#FBACB3] font-bold"
-			onClick = {() => {
-				setLook(true);
-			}}>
-			find a player
-			</button>}
-
-			{look && <button 
-			className=" w-full max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-[#fc2e2e] py-2 w-[120px] rounded-[10px] hover:bg-[#f01f2d] font-bold"
-			onClick = {() => {
-				setLook(false);
-			}}>
-			cancel
-			</button>}
+				{look === 1 && <button 
+				className=" max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-[#fc2e2e] py-2 w-[120px] rounded-[10px] hover:bg-[#f01f2d] font-bold"
+				onClick = {() => {
+					socket.emit('cancel-looking', globalData.username);
+					setLook(0);
+				}}>
+				cancel
+				</button>}
+			</div>}
 		</div>
 		)
 }
