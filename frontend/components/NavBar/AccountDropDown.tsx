7b4af320dc58948a6e4451066ad6c1623234c492
mohamed elazhari <mazhari@student.1337.ma>
2023-08-24 19:46:18 +0100

    makeing searchbar responsive

--- frontend/components/NavBar/AccountDropDown.tsx
+++ frontend/components/NavBar/AccountDropDown.tsx
@@ -3,9 +3,32 @@
 import React from 'react'
 import Image from 'next/image'
 import NavLink from './NavLink';
-import { useState } from 'react';
-import Settings from '@/components/settings/Settings';
+import { useState, useEffect } from 'react';
+import useCloseOutSide from '@/hookes/useCloseOutSide';
 
+interface Props {
+  src: string;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+function DropDown({src, setIsOpen}: Props) {
+
+  const {divref} = useCloseOutSide({setIsOpen});
+
+  return (
+    <div
+      ref={divref}
+      className='hidden md:flex flex-col justify-around  absolute top-[56px] right-0 w-[56px] h-[160px]'
+    >
+                          <NavLink route={'/profile'} src={src} alt={'profile'} setIsOpen={setIsOpen}/>
+                          {/* <button className='grid place-content-center h-[55px] w-[56px] hover:bg-light-gray' onClick={ () => setIsSettingsOpen(!isSettingsOpen)}>
+                            <Image src={'/icons/navBar/settings.svg'} alt={'settings'} width={28} height={28} /> 
+                          </button> */}
+                          <NavLink route={'/settings'} src={'/icons/navBar/settings.svg'} alt={'settings'} setIsOpen={setIsOpen}/>
+                          <NavLink route={'/'} src={'/icons/navBar/logout.svg'} alt={'logout'} setIsOpen={setIsOpen}/>
+                      </div>
+  )
+}
 
 const AccountDropDown = () => {
 
@@ -14,6 +37,8 @@ const AccountDropDown = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
 
+
+
   return (
     <>
         <button className='hidden md:grid place-content-center mr-[14px]' onClick={() => setIsOpen(!isOpen)}>
@@ -21,14 +46,7 @@ const AccountDropDown = () => {
           {!isOpen && <Image src={src} alt={"user image"} width={28} height={28} />}
         </button>
         {
-            isOpen && (<div className='hidden md:flex flex-col justify-around  absolute top-[56px] right-0 w-[56px] h-[160px]'>
-                          <NavLink route={'/profile'} src={src} alt={'profile'} setIsOpen={setIsOpen}/>
-                          {/* <button className='grid place-content-center h-[55px] w-[56px] hover:bg-light-gray' onClick={ () => setIsSettingsOpen(!isSettingsOpen)}>
-                            <Image src={'/icons/navBar/settings.svg'} alt={'settings'} width={28} height={28} /> 
-                          </button> */}
-                          <NavLink route={'/settings'} src={'/icons/navBar/settings.svg'} alt={'settings'} setIsOpen={setIsOpen}/>
-                          <NavLink route={'/'} src={'/icons/navBar/logout.svg'} alt={'logout'} setIsOpen={setIsOpen}/>
-                      </div>)
+            isOpen && <DropDown src={src} setIsOpen={setIsOpen}/>
         }
         {/* {
           isSettingsOpen && <Settings/>
--- frontend/components/common/SearchBar.tsx
+++ frontend/components/common/SearchBar.tsx
@@ -1,56 +1,14 @@
-"use client";
-import React, { useState } from 'react';
-import { useQuery } from "@tanstack/react-query";
-import axios from 'axios';
-import { userDto } from '@/dto/userDto';
-import Link from 'next/link';
-import Image from 'next/image';
-
-const SearchBar = () => {
-  const [search, setSearch] = useState<string>("");
-  const [isFocused, setIsFocused] = useState<boolean>(false);
-
-  const users = useQuery({
-    queryKey: ['search', search],
-    queryFn: async () => {
-      const { data } = await axios.get(`http://localhost:8000/users/search/${search}`, { withCredentials: true });
-      console.log(data);
-      return data;
-    }
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
+interface Props {
+    setIsOpen: (isFocused: boolean) => void;
+    setSearchValue: (searchValue: string) => void;
   }
-
-  const User = ({ user }: { user: userDto }) => {
+  
+const SearchBar = ({ setIsOpen, setSearchValue }: Props) => {
+  
     return (
-      <div className="flex items-center gap-4 bg-dark-gray px-4 py-2 rounded-xl">
-        <Image className="sm:w-[48px] sm:h-[48px] rounded-full self-center" src={user.image} width={36} height={36} alt="user image" />
-        <h3 className="text-[12px] sm:text-[24px]">{user.name}</h3>
-      </div>
+      <input className="h-8 w-full text-sm rounded-xl text-black focus:outline-0 focus:border-[2px] hover:opacity-60" type="text" placeholder="Search" name="search"
+      onFocus={() => setIsOpen(true)} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchValue(e.target.value)} />
     )
-  }
-
-  return (
-    <div className='md:block relative'>
-      <input className="h-8 w-64 text-sm rounded-xl text-black focus:outline-0 focus:border-[2px] hover:opacity-60" type="text" placeholder="Search" name="search"
-        onFocus={() => setIsFocused(true)} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} />
-      { users.isloading && <p>loading...</p>}
-      {
-        isFocused && (search.length != 0) &&  <div className='absolute top-[44px] w-64 max-h-56 bg-light-gray rbg-light-gray p-4 flex flex-col gap-1 overflow-y-scroll rounded-2xl'>
-                  {
-                    users.data?.users?.length === 0 ? <p className="text-center">No user found</p> :
-                    users.data?.users?.map((user: userDto) => (
-                      <Link  href={`/profile/${user.name}`}>
-                        <User user={user} />
-                      </Link>
-                    ))
-                  }
-                </div>
-      }
-      </div>
-  )
 }
 
 export default SearchBar;
