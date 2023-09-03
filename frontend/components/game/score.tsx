6c5ea5a8ae09fdd081a829db7ac8acf087617a4b
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-09-03 01:38:05 +0100

    robot game logic done, still need to fix the frontend

--- frontend/components/game/score.tsx
+++ frontend/components/game/score.tsx
@@ -41,26 +41,41 @@ function MePlayer({ score, name} : pScore) {
 }
 
 function OtherPlayer({ score, name } : pScore) {
-	const {data, isLoading} = useQuery({
-		queryKey: ['scoreright'],
-		queryFn: async ()=> {
-		  const {data} = await axios.get(`http://localhost:8000/users/byUsername/${name}`, { withCredentials: true })
-		  return data;
-		}
-	  });
-	if (isLoading) return <div>Loading...</div>;
-	else{
+	if (name == "robot"){
 		return (
 			<div className="flex items-center gap-2">
+				<div className="flex flex-col items-center w-[80px] h-[80px]">
+					<Image src="/game/computer.svg" width={100} height={100} alt="#" className="w-full h-full rounded-full"/>
+					<h1 className="text-[#F2F2F2] font-bold">{name}</h1>
+				</div>
 				<h1>
 					score: {score}
 				</h1>
-				<div className="flex flex-col items-center w-[80px] h-[80px]">
-					<Image src={data.image} width={100} height={100} alt="#" className="w-full h-full rounded-full"/>
-					<h1 className="text-[#F2F2F2] font-bold">{data.name}</h1>
-				</div>
 			</div>
 		);
+	}
+	else{
+		const {data, isLoading} = useQuery({
+			queryKey: ['scoreright'],
+			queryFn: async ()=> {
+			const {data} = await axios.get(`http://localhost:8000/users/byUsername/${name}`, { withCredentials: true })
+			return data;
+			}
+		});
+		if (isLoading) return <div>Loading...</div>;
+		else{
+			return (
+				<div className="flex items-center gap-2">
+					<h1>
+						score: {score}
+					</h1>
+					<div className="flex flex-col items-center w-[80px] h-[80px]">
+						<Image src={data.image} width={100} height={100} alt="#" className="w-full h-full rounded-full"/>
+						<h1 className="text-[#F2F2F2] font-bold">{data.name}</h1>
+					</div>
+				</div>
+			);
+		}
 	}
 }
 
@@ -68,8 +83,8 @@ function PlayersScore({ left, right, leftPlayer, rightPlayer } : score) {
 
 	return (
 	  <div className="flex justify-between absolute top-[160px] left-[100px] right-[100px]">
-		<MePlayer score={left} name={leftPlayer}/>
-		<OtherPlayer score={right} name={rightPlayer}/>
+		<OtherPlayer score={left} name={leftPlayer}/>
+		<MePlayer score={right} name={rightPlayer}/>
 	  </div>
 	);
 }
