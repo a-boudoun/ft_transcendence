eb5be3545bfa831444bd60bd3629a8945ec62b31
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-09-03 04:14:15 +0100

    adding line int the middle

--- frontend/app/(login)/game/page.tsx
+++ frontend/app/(login)/game/page.tsx
@@ -6,11 +6,11 @@ export default function Gamehome(){
 	const router = useRouter();
 	return (
 		<div className="h-screen p-[56px] flex items-center justify-center  bg-dark-gray">
-			<div className="h-[500px] w-[500px] flex flex-col items-center justify-center space-y-20 bg-[#4D566A] rounded-[30px]"> 
-  				<button className="text-white text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/maps')}}>
+			<div className="h-[500px] w-[500px] flex flex-col items-center justify-center space-y-20 bg-[#384259] rounded-[30px]"> 
+  				<button className="text-white font-bold text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/maps')}}>
    				 online game
   				</button>
-  				<button className="text-white text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/ai-game')}}>
+  				<button className="text-white font-bold text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]" onClick={() => {router.push('http://localhost:3000/game/ai-game')}}>
    				 play with AI
   				</button>
 			</div>
