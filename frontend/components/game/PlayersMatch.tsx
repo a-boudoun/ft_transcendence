cf876b4fcb18fedab8fe45e5a8e97a465de2b34b
mohamed elazhari <mazhari@student.1337.ma>
2023-09-05 23:55:21 +0100

    adding unBlock user

--- frontend/components/game/PlayersMatch.tsx
+++ frontend/components/game/PlayersMatch.tsx
@@ -17,7 +17,7 @@ function LeftPlayer(){
 	const {data, isLoading} = useQuery({
 		queryKey: ['user'],
 		queryFn: async ()=> {
-		  const {data} = await axios.get('http://localhost:8000/users/me', { withCredentials: true })
+		  const {data} = await axios.get('http://localhost:8000/users/getUser/me', { withCredentials: true })
 		  return data;
 		}
 	  });
--- frontend/components/settings/ChangeNameImage.tsx
+++ frontend/components/settings/ChangeNameImage.tsx
@@ -21,7 +21,7 @@ const ChangeNameImage = () => {
     const User = useQuery({
         queryKey: ['user'],
         queryFn: async ()=> {
-            const {data} = await axios.get('http://localhost:8000/users/me', { withCredentials: true })
+            const {data} = await axios.get('http://localhost:8000/users/getUser/me', { withCredentials: true })
             return data;
         }
     });
