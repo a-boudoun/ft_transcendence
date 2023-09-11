ad3c13d7cad7dafb2f56e1b3ee545b87e4662745
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-09-11 19:23:47 +0100

    football game working

--- frontend/components/game/footGame.tsx
+++ frontend/components/game/footGame.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from "react";
-import {Engine, Render, Body, Composite} from "matter-js";
+import {Engine, Render, Body, Composite, Bodies} from "matter-js";
 import socket from "../socketG";
-import { drawRect, drawCircle } from "./draw";
 
 
 interface Prop{
@@ -80,21 +79,52 @@ function FootGame({roomid, me, RightPlayer} : Prop){
 					height: H,
 					pixelRatio: 1,
 					wireframes: false,
-					background: "#7AC7C4",
+					background: "/game/pitch2.jpg",
 				}
 			});
 
-			const rightBoard = drawRect(W - 35, H / 2, 20, 120, '#D46077');
-			const leftBoard = drawRect(35, H / 2, 20, 120, '#D46077');
+			const rightBoard = Bodies.rectangle(W - 35, H / 2, 20, 120,{
+				isStatic: true,
+				render: {
+					sprite: {
+						texture: '/game/rightglove.png',
+						xScale: 0.045,
+						yScale: 0.05,
+					}
+			},
+			});
+			const leftBoard = Bodies.rectangle(35, H / 2, 20, 120,{
+				isStatic: true,
+				render: {
+					sprite: {
+						texture: '/game/leftglove.png',
+						xScale: 0.045,
+						yScale: 0.05,
+					}
+			},
+			});
 
-			const ball = drawCircle(W / 2, H / 2, 15, '#384259');
+			const ball = 	Bodies.circle(W / 2, H / 2, 15,{
+				restitution: 1, // Make the ball fully elastic
+				friction: 0, // Remove friction
+				frictionAir: 0, // Remove air friction
+				inertia: Infinity, // prevent ball from slowing down
+				render: {
+					// fillStyle: color,
+					sprite: {
+						texture: 'https://cdn.shopify.com/s/files/1/0009/2733/4466/products/755957ca0e8d95bb1d4e9ac2c8083de9.png?v=1539975508',
+						xScale: 0.04,
+						yScale: 0.04,
+					}
+			},
+			});
 			Composite.add(engine.world, [ball, rightBoard, leftBoard]);
 			document.addEventListener('keyup', handlekeyUp);
 			document.addEventListener('keydown', handleKeyDown);
 			Render.run(render);
 			
 			socket.on('sound', () => {
-				const audio = new Audio('/game/bounce.mp3');
+				const audio = new Audio('/game/football.mp3');
 				audio.play();
 			});
 			
@@ -156,7 +186,7 @@ function FootGame({roomid, me, RightPlayer} : Prop){
 					  windowHeight = window.innerHeight;
 					  scaleFactor = Math.min(windowWidth / canvasWidth, windowHeight / canvasHeight);
 					  scalex = scaleFactor > 1 ? 1 : scaleFactor * 0.95;
-					  scaley = scaleFactor > 0.95 ? 1 : scaleFactor * 0.85; // adding the navbar height
+					  scaley = scaleFactor > 1 ? 1 : scaleFactor * 0.85; // adding the navbar height
 					  setSx(scalex);
 					  setSy(scaley);
 					}
@@ -171,12 +201,9 @@ function FootGame({roomid, me, RightPlayer} : Prop){
 					className="h-[900px] w-[1700px] mt-20 relative"
 					style={{
 				transform: `scale(${sx}, ${sy})`,
-			}}
-			>
-			<div className="h-full w-1 bg-white absolute left-1/2 transform -translate-x-1/2">
-			<div className="bg-white w-full" style={{backgroundImage: 'repeating-linear-gradient(2deg, transparent, transparent 40px, white 40px, white 80px)'}}></div>
-			</div>
-		</div>
-	);
+				}}
+				>
+					</div>
+				);
 }
 export default FootGame;
