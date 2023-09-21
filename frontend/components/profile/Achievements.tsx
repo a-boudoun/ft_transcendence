7d2755234eb6c1ace01531664941b17b2065b81d
moelazhari <mazhari@student.1337.ma>
2023-09-21 23:55:22 +0100

    adding  archievements

--- frontend/components/profile/Achievements.tsx
+++ frontend/components/profile/Achievements.tsx
@@ -1,6 +1,6 @@
 import Title from "./Title";
 
-const Achievements = () => {
+const Achievements = ({user} : {user : any}) => {
   return (
     <div className="flex flex-col grow rounded-3xl shadow-2xl xl:bg-white xl:bg-opacity-20 xl:ackdrop-blur-lg xl:drop-shadow-lg">
       <div className="hidden xl:block rounded-t-3xl p-4 ">
@@ -10,8 +10,18 @@ const Achievements = () => {
           src="/icons/profile/Achievements.svg"
         />
       </div>
-      <div className="grow grid place-content-center  xl:rounded-b-3xl">
-        achievements
+      <div className="flex flex-col gap-4 p-4 xl:rounded-b-3xl">
+        <div className="flex flex-col p-4 bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-2xl">
+          <h3 className="text-3xl ">YOU ARE HER</h3>
+          <p className="text-[12px] text-blue"> you login for the first time</p>
+        </div>
+        {
+          user.wins > 0 &&
+          <div className="flex flex-col  p-4 bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-2xl">
+            <h3 className="text-3xl "> DASR </h3>
+            <p className="text-[12px] text-blue"> you won your first game </p>
+          </div>
+        }
       </div>
     </div>
   );
