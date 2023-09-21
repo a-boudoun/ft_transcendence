575aa9e1f01b38386d5fe8bcf25a210b237e3caf
mohamed elazhari <mazhari@student.1337.ma>
2023-09-21 20:16:45 +0100

    celing code

--- frontend/components/profile/Matches.tsx
+++ frontend/components/profile/Matches.tsx
@@ -2,77 +2,82 @@
 
 import Title from "@/components/profile/Title";
 import { useQuery } from "@tanstack/react-query";
-import axios from '@/apis/axios';
-import Image from 'next/image';
+import axios from "@/apis/axios";
+import Image from "next/image";
 import Link from "next/link";
 import { userDto } from "@/dto/userDto";
 interface MatchProps {
-    winner: userDto,
-    loser: userDto,
-    loserScore: number,
+  winner: userDto;
+  loser: userDto;
+  loserScore: number;
 }
 
-const Match = ({match, id} : {match : MatchProps, id: number}) => {
+const Match = ({ match, id }: { match: MatchProps; id: number }) => {
+  const border =
+    match.winner.id === id ? "border-2 border-blue" : "border-2 border-red";
 
-    
-    const border = match.winner.id === id ? 'border-2 border-blue' : 'border-2 border-red';
-    
+  return (
+    <div
+      className={`flex justify-between items-center p-4 rounded-xl bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg ${border} `}
+    >
+      <div className="flex flex-col items-center gap-4">
+        <Link href={`/profile/${match.winner.id}`}>
+          <Image
+            className="rounded-full"
+            src={match.winner.image}
+            width={64}
+            height={64}
+            alt="user image"
+          />
+        </Link>
+        <h3>{match.winner.id}</h3>
+      </div>
+      <span className="grow text-3xl mb-8">{`5 - ${match.loserScore}`} </span>
+      <div className="flex flex-col items-center gap-4">
+        <Link href={`/profile/${match.loser.id}`}>
+          <Image
+            className="rounded-full"
+            src={match.loser.image}
+            width={64}
+            height={64}
+            alt="user image"
+          />
+        </Link>
+        <h3>{match.loser.id}</h3>
+      </div>
+    </div>
+  );
+};
+
+const DisplayMatchs = ({ id }: { id: number }) => {
+  const Matchs = useQuery({
+    queryKey: ["matches"],
+    queryFn: async () => {
+      const { data } = await axios.get(`/gameHistory/getHistory/${id}`);
+      return data;
+    },
+  });
+  if (Matchs.isLoading) return <div> loading... </div>;
+  else {
     return (
-        <div className={`flex justify-between items-center p-4 rounded-xl bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg ${border} `}>
-            <div className="flex flex-col items-center gap-4">
-            <Link href={`/profile/${match.winner.id}`} >
-                <Image className="rounded-full"  src={match.winner.image}  width={64}  height={64}   alt="user image"/>
-            </Link>
-                <h3>{match.winner.id}</h3>
-            </div>
-            <span className="grow text-3xl mb-8">{`5 - ${match.loserScore}`} </span>
-            <div className="flex flex-col items-center gap-4">
-            <Link href={`/profile/${match.loser.id}`} >
-                <Image className="rounded-full"  src={match.loser.image}  width={64}  height={64}   alt="user image"/>
-            </Link>
-                <h3>{match.loser.id}</h3>
-            </div>
-        </div>
-    )
-}
+      <div className="grow flex flex-col gap-1 p-4 overflow-y-scroll  xl:rounded-b-3xl">
+        {Matchs.data.map((match: any) => {
+          return <Match match={match} id={id} />;
+        })}
+      </div>
+    );
+  }
+};
 
-const DisplayMatchs = ({id} : {id : number}) => {
-    
-    const Matchs = useQuery({
-        queryKey: ['matches'],
-        queryFn: async ()=> {
-            const { data } = await axios.get(`/gameHistory/getHistory/${id}`);
-            return data;
-        }
-    });
-    if (Matchs.isLoading) 
-    return (<div> loading... </div>)
-    else
-    {
-        return (
-            <div className="grow flex flex-col gap-1 p-4 overflow-y-scroll  xl:rounded-b-3xl">
-            {
-                Matchs.data.map((match: any) => {
-                    return (
-                        <Match match={match} id={id} />
-                        );
-                    })
-                }
-            </div>
-        )
-    }
-}
-
-const MatchesHistory = ({id} : {id : number}) => {
-    
-    return (
-        <div className="max-h-[668px] flex flex-col grow xl:rounded-3xl xl:shadow-2xl xl:bg-white xl:bg-opacity-20 xl:ackdrop-blur-lg xl:drop-shadow-lg">
-                <div className="hidden xl:block rounded-t-3xl  p-4 ">
-                    <Title isActive={true} str='Matches' src='/icons/profile/Matches.svg'/>
-                </div>
-                <DisplayMatchs id={id} />
-            </div>
-    )
+const MatchesHistory = ({ id }: { id: number }) => {
+  return (
+    <div className="max-h-[668px] flex flex-col grow xl:rounded-3xl xl:shadow-2xl xl:bg-white xl:bg-opacity-20 xl:ackdrop-blur-lg xl:drop-shadow-lg">
+      <div className="hidden xl:block rounded-t-3xl  p-4 ">
+        <Title isActive={true} str="Matches" src="/icons/profile/Matches.svg" />
+      </div>
+      <DisplayMatchs id={id} />
+    </div>
+  );
 };
 
 export default MatchesHistory;
--- frontend/components/profile/MidBottom.tsx
+++ frontend/components/profile/MidBottom.tsx
@@ -3,40 +3,46 @@
 import PercentageCircle from "./PercentageCircle";
 import PercentageLine from "./PercentageLine";
 
-const Stats = ({user} : {user : any}) => {
-
+const Stats = ({ user }: { user: any }) => {
   const games = user.wins + user.loses;
 
-    return(
-      <div className='h-full flex flex-col justify-center gap-20 xl:z-40'>
-        <div className="w-full flex flex-col gap-4 p-4">
-          <PercentageLine  value={user.level}/>
-          <div className="flex justify-center gap-4">
-            <h3 className='text-blue text-lg'>Level</h3>
-            <span className='font-bold text-lg'>{user.level}</span>
-            <h3 className='text-blue text-lg'>Xp</h3>
-            <span className='font-bold text-lg'>{user.XP}</span>
-          </div>
+  return (
+    <div className="h-full flex flex-col justify-center gap-20 xl:z-40">
+      <div className="w-full flex flex-col gap-4 p-4">
+        <PercentageLine value={user.level} />
+        <div className="flex justify-center gap-4">
+          <h3 className="text-blue text-lg">Level</h3>
+          <span className="font-bold text-lg">{user.level}</span>
+          <h3 className="text-blue text-lg">Xp</h3>
+          <span className="font-bold text-lg">{user.XP}</span>
         </div>
-        <div>
-          <div className=''>
-            <span className='font-bold text-3xl'>{games}</span>
-            <h3 className='text-blue text-lg'>Total games</h3>
+      </div>
+      <div>
+        <div className="">
+          <span className="font-bold text-3xl">{games}</span>
+          <h3 className="text-blue text-lg">Total games</h3>
+        </div>
+        <div className="w-full flex justify-around">
+          <div className="max-w-[120px] sm:max-w-[200px] flex flex-col gap-2">
+            <PercentageCircle
+              value={user.wins}
+              percentage={games ? (user.wins / games) * 100 : 0}
+              color={"#7AC7C4"}
+            />
+            <h3 className="text-blue">Wins</h3>
           </div>
-          <div className="w-full flex justify-around">
-            <div className="max-w-[120px] sm:max-w-[200px] flex flex-col gap-2">
-                <PercentageCircle value={user.wins} percentage={games ? (user.wins / games) * 100 : 0} color={'#7AC7C4'} />
-                <h3 className='text-blue'>Wins</h3>
-              </div>
-            <div className="flex flex-col gap-2">
-              <PercentageCircle value={user.loses} percentage={games ? (user.loses / games) * 100 : 0} color={'#EA5581'}/>
-              <h3 className='text-red'>Loses</h3>
-            </div>
+          <div className="flex flex-col gap-2">
+            <PercentageCircle
+              value={user.loses}
+              percentage={games ? (user.loses / games) * 100 : 0}
+              color={"#EA5581"}
+            />
+            <h3 className="text-red">Loses</h3>
           </div>
         </div>
       </div>
-    )
-
-}
+    </div>
+  );
+};
 
 export default Stats;
