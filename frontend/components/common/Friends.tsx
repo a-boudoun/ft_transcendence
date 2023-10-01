7afa6b94e0995ecc8997339adab7037eec552d65
moelazhari <mazhari@student.1337.ma>
2023-10-01 21:37:17 +0100

    adding challenge component

--- frontend/components/common/Friends.tsx
+++ frontend/components/common/Friends.tsx
@@ -8,6 +8,7 @@ import { useQuery } from "@tanstack/react-query";
 import socket from "../socketG";
 import { MessagesSquare, Gamepad2 } from "lucide-react";
 import { useState } from "react";
+import useCloseOutSide from "@/hookes/useCloseOutSide";
 
 const Friends = ({ id, isMe }: { id: number; isMe: boolean }) => {
   const { data, isLoading } = useQuery({
@@ -22,7 +23,7 @@ const Friends = ({ id, isMe }: { id: number; isMe: boolean }) => {
   else {
     return (
       <div
-        className={"h-full flex flex-col gap-1 overflow-y-scroll rounded-2xl"}
+        className={"h-full flex flex-col gap-1 rounded-2xl overflow-scroll"}
       >
         {data.map((friend: userDto) => {
           return <Friend key={friend.id} user={friend} isMe={isMe} />;
@@ -74,32 +75,82 @@ export const Friend = ({ user, isMe }: { user: userDto; isMe: boolean }) => {
               <Gamepad2 size={32} color="#7ac7c4" strokeWidth={1.5} />
             </button>
           </div>
-          {isOpen && <ChallengeDropDown id={user.id} />}
+          {isOpen && <ChallengeDropDown id={user.id} setIsOpen={setIsOpen} />}
         </>
       )}
     </div>
   );
 };
 
-const ChallengeDropDown = ({ id }: { id: number }) => {
+interface ChallengeDropDownProps {
+  id: number;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+const ChallengeDropDown = ({ id, setIsOpen }: ChallengeDropDownProps) => {
+  const { divref } = useCloseOutSide({ setIsOpen });
+
   return (
-      <div className="absolute top-12 right-4 flex flex-col gap-4 rounded-2xl  bg-black bg-opacity-50 ackdrop-blur-lg drop-shadow-lg p-4">
-        <h3 className="text-blue"> challenge game of: </h3>
-        <div className="flex gap-4">
-          <div className="w-[200px] flex gap-4 items-center bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-2xl p-2">
-            <div className="rounded-full h-[38px] w-[38px] overflow-hidden">
-              <Image className="h-full w-full object-cover object-center"
-                src="/game/football-map-select.svg"
-                width={1000}
-                height={1000}
-                alt="rock"
-              />
-            </div>
-            <span>futball</span>
+    <div
+      ref={divref}
+      className="absolute top-12 right-4 flex flex-col gap-4 rounded-2xl  bg-black bg-opacity-50 ackdrop-blur-lg drop-shadow-lg p-4"
+    >
+      <h3 className="text-blue"> challenge game of: </h3>
+      <div className="flex flex-col gap-2">
+        <button
+          className="w-[200px] flex gap-4 items-center bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-2xl p-2 hover:opacity-50"
+          onClick={() => {
+            socket.emit("invite-freind", { id: id, game: "football" });
+          }}
+        >
+          <div className="rounded-full h-[38px] w-[38px] overflow-hidden">
+            <Image
+              className="h-full w-full object-cover object-center"
+              src="/game/football-map-select.svg"
+              width={1000}
+              height={1000}
+              alt="football"
+            />
           </div>
-        </div>
+          <span>football</span>
+        </button>
+        <button
+          className="w-[200px] flex gap-4 items-center bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-2xl p-2 hover:opacity-50"
+          onClick={() => {
+            socket.emit("invite-freind", { id: id, game: "space"});
+          }}
+        >
+          <div className="rounded-full h-[38px] w-[38px] overflow-hidden">
+            <Image
+              className="h-full w-full object-cover object-center"
+              src="/game/space-map-select.svg"
+              width={1000}
+              height={1000}
+              alt="space"
+            />
+          </div>
+          <span>space</span>
+        </button>
+        <button
+          className="w-[200px] flex gap-4 items-center bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-2xl p-2 hover:opacity-50"
+          onClick={() => {
+            socket.emit("invite-freind", { id: id, game: "ping pong" });
+          }}
+        >
+          <div className="rounded-full h-[38px] w-[38px] overflow-hidden">
+            <Image
+              className="h-full w-full object-cover object-center"
+              src="/game/default-map-select.svg"
+              width={1000}
+              height={1000}
+              alt="ping pong"
+            />
+          </div>
+          <span>ping pong</span>
+        </button>
       </div>
+    </div>
   );
 };
 
-// () => {socket.emit('invite-freind', user.id)}
+// () => {socket.emit('invite-freind', {id: user.id, game: 'football'})}
