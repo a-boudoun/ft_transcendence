8b08f28762e65393211e9c7b15e45bec79b4e248
abderrahim boudounit <abderrahimboudounit@gmail.com>
2023-10-02 22:24:29 +0100

    fixing Id not fetched in match rout

--- frontend/app/(login)/game/match/page.tsx
+++ frontend/app/(login)/game/match/page.tsx
@@ -1,63 +1,52 @@
 'use client';
 
 import { LoadingPlayer, LeftPlayer } from '@/components/game/PlayersMatch';
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import socket from '@/components/socketG';
 import axios from '@/apis/axios';
+import { useQuery } from "@tanstack/react-query";
 
 import  Game  from '@/components/game/fullGame';
 
 export default function MatchPlayers() {
-	const [playerId, setPlayerId] = useState<string>('');
-	const [isPlayerFetched, setIsPlayerFetched] = useState<boolean>(false);
 	const [gameStart, setGameStart] = useState<boolean>(false);
-  
-	useEffect(() => {
-	  const fetchData = async () => {
-		try {
-		  const { data } = await axios.get(`/users/getUser/me`);
-		  if (data) {
-			setPlayerId(data.id);
-			setIsPlayerFetched(true);
-		  }
-		} catch (error) {
-		  console.error('Error fetching user data:', error);
-		}
-	  };
-	  fetchData();
-	}, []);
-	
-	useEffect(() => {
-		if (isPlayerFetched) {
-			socket.emit('player-status', playerId);
-		}
-		socket.on('player-status', (status : string)=> {
-			if (status === 'not-looking'){
-				socket.emit('looking-for-match', playerId)
-			}
-			else
-				setGameStart(true);
-		});
-		return () => {
-		  socket.off('player-status');
-		};
-	}, [isPlayerFetched, playerId]);
 
+
+	const user = useQuery({
+		queryKey: ["user", "me"],
+		queryFn: async () => {
+			const { data } = await axios.get(`/users/getUser/me`);
+			return data;
+		},
+		onSuccess: () => {
+			socket.emit('player-status', user.data.id);
+
+			socket.on('player-status', (status : string)=> {
+				if (status === 'not-looking'){
+					socket.emit('looking-for-match', user.data.id)
+				}
+				else{
+					setGameStart(true);
+				}
+			});
+		}
+	  });
+	
 	return (
 
 		<>
-		{ !gameStart && <div className = 'flex flex-col items-center justify-center w-screen h-screen gap-2'>
-		<main className=" pt-[56px] sm:p-10 sm:pt-[96px] sm:flex sm:justify-center">
-			<div className = 'flex flex-col gap-8 sm:flex-row bg-[#4f5a748a] items-center justify-between p-8 rounded-2xl'>
-				<LeftPlayer/>
-				<h1 className ='text-5xl font-serif'>VS</h1>
-				<LoadingPlayer setGame={setGameStart}/>
-			</div>
-		</main>
-		</div>}
-		{gameStart && <Game 
-			meId = {playerId}
-		/>}
+			{ !gameStart && <div className = 'flex flex-col items-center justify-center w-screen h-screen gap-2'>
+			<main className=" pt-[56px] sm:p-10 sm:pt-[96px] sm:flex sm:justify-center">
+				<div className = 'flex flex-col gap-8 sm:flex-row bg-[#4f5a748a] items-center justify-between p-8 rounded-2xl'>
+					<LeftPlayer/>
+					<h1 className ='text-5xl font-serif'>VS</h1>
+					<LoadingPlayer setGame={setGameStart}/>
+				</div>
+			</main>
+			</div>}
+			{ gameStart  && (user.isLoading ?  <div>loading... </div>  :
+			<Game meId = {user.data.id} /> )
+			}
 		</>
-		)
+		);
 }
