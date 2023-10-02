2846ed666512dd92f9d41dd8028427e794947bda
moelazhari <mazhari@student.1337.ma>
2023-10-02 02:56:40 +0100

    fixing game and maps pages frontend

--- frontend/app/(login)/game/maps/page.tsx
+++ frontend/app/(login)/game/maps/page.tsx
@@ -4,61 +4,106 @@ import { useRouter } from "next/navigation";
 
 import Image from "next/image";
 
-export default function Maps(){
-	const router = useRouter();
-	const [selectedMap, setSelectedMap] = useState<string>("default");
+export default function Maps() {
+  const router = useRouter();
+  const [selectedMap, setSelectedMap] = useState<string>("default");
 
+  const setmapToLocalStorage = (map: string) => {
+    setSelectedMap(map);
+    localStorage.setItem("map", map);
+  };
 
-	const setmapToLocalStorage = (map: string) => {
-		setSelectedMap(map);
-		localStorage.setItem("map", map);
-	}
+  useEffect(() => {
+    const map: string | null = localStorage.getItem("map");
 
+    if (map) {
+      setSelectedMap(map);
+    }
+  }, [selectedMap]);
 
-	useEffect(() => {
-		const map: string | null = localStorage.getItem("map");
-
-		if (map) {
-			setSelectedMap(map);
-		}
-	}, [selectedMap]);
-
-	return (
-		<main className="h-full w-full pt-[56px] sm:p-10 sm:pt-[96px] sm:flex sm:justify-center gap-8">
-			<div className="flex flex-col items-center gap-8 pt-8 sm:gap-16 sm:pt-32">
-				<div className="bg-[#4b5468a0]  rounded-[10px] w-[300px] py-4">
-					<h3 className="text-[#B5B3BD] tracking-wide [word-spacing:3px] font-medium text-3xl"> selet the map </h3>
-				</div>
-				<div className="flex justify-center gap-8 items-center flex-col sm:flex-row w-full">
-
-						<div
-							className={selectedMap === "football-mode" ? "w-full max-w-[320px] border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
-							:"w-full max-w-[320px] hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px]"}
-							onClick={() => setmapToLocalStorage("football-mode")}>
-							<Image width={400} height={400} alt="#" src="/game/football-map-select.svg" className="h-full w-full"/>
-							<p className="text-white text-center mt-[10px] font-bold">football-mode</p>
-						</div>
-						<div 
-							className={selectedMap === "space-mode" ? "w-full max-w-[320px] border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
-							:"w-full max-w-[320px] hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px]"}
-							onClick={() => setmapToLocalStorage("space-mode")}>
-							<Image width={400} height={400} alt="#" src="/game/space-map-select.svg" className="h-full w-full"/>
-							<p className="text-white text-center mt-[10px] font-bold">space-mode</p>
-						</div>
-						<div 
-							className={selectedMap === "default" ? "w-full max-w-[320px] border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
-							:"w-full max-w-[320px] hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px]"}
-							onClick={() => setmapToLocalStorage("default")}>
-							<Image width={400} height={400} alt="#" src="/game/default-map-select.svg" className="h-full w-full"/>
-							<p className="text-white text-center mt-[10px] font-bold">default</p>
-						</div>
-				</div>
-					<button
-						className=" w-full max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-red  py-2 rounded-[10px] hover:bg-[#FBACB3] font-bold" onClick={() => {router.push('/game/match')}}
-						>
-						Next
-					</button>
+  return (
+    <main className="h-full bg-bg bg-cover w-full grid place-content-center pt-[56px]  ">
+      <div className="h-fit flex flex-col items-center gap-8 p-8 sm:gap-16  sm:bg-white sm:bg-opacity-20 sm:ackdrop-blur-lg sm:drop-shadow-lg sm:rounded-3xl">
+        <div className="bg-[#4b5468a0]  rounded-[10px] w-[300px] py-4">
+          <h3 className="text-[#B5B3BD] tracking-wide [word-spacing:3px] font-medium text-3xl">
+            {" "}
+            selet the map{" "}
+          </h3>
+        </div>
+        <div className="flex justify-center gap-8 items-center flex-col sm:flex-row w-full">
+          <button
+            className={`w-full flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE] rounded-b-[8px] ${
+				selectedMap === "football-mode"
+                ? "border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
+                : ""
+            }`}
+			onClick={() => setmapToLocalStorage("football-mode")}
+          >
+			<div className="max-w-[180px] sm:max-w-[260px]">
+				<Image
+				width={260}
+				height={260}
+				alt="#"
+				src="/game/football-map-select.svg"
+				className="h-full w-full"
+				/>
 			</div>
-		</main>
-	)
+            <p className="text-white text-center mt-[10px] font-bold">
+              football-mode
+            </p>
+          </button>
+          <button
+           className={`w-full flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE] rounded-b-[8px] ${
+			   selectedMap === "space-mode"
+			   ? "border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
+			   : ""
+			}`}
+			onClick={() => setmapToLocalStorage("space-mode")}
+          >
+			<div className="max-w-[180px] sm:max-w-[260px]">
+				<Image
+				width={260}
+				height={0}
+				alt="#"
+				src="/game/space-map-select.svg"
+				className="h-full w-full"
+				/>
+			</div>
+            <p className="text-white text-center mt-[10px] font-bold">
+              space-mode
+            </p>
+          </button>
+          <button
+          className={`w-full flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE]  rounded-b-[8px] ${
+			selectedMap === "default"
+			? "border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
+			: ""
+		 }`}
+            onClick={() => setmapToLocalStorage("default")}
+          >
+			<div className="max-w-[180px] sm:max-w-[260px]">
+				<Image
+				width={260}
+				height={0}
+				alt="#"
+				src="/game/default-map-select.svg"
+				className="h-full w-full"
+				/>
+			</div>
+            <p className="text-white text-center mt-[10px] font-bold">
+              default
+            </p>
+          </button>
+        </div>
+        <button
+          className=" w-full max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-red  py-2 rounded-[10px] hover:bg-[#FBACB3] font-bold"
+          onClick={() => {
+            router.push("/game/match");
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </main>
+  );
 }
--- frontend/app/(login)/game/page.tsx
+++ frontend/app/(login)/game/page.tsx
@@ -1,19 +1,14 @@
 import Link from "next/link";
 
 export default function Gamehome() {
-	console.log("gamehome");
   return (
     <main className="h-full grid place-content-center">
-      <div className="h-[500px] w-[500px] flex flex-col items-center justify-center gap-20 bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-3xl ">
-        <Link href="/game/maps">
-          <button className="text-white font-bold text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]">
-            online game
-          </button>
+      <div className="font-bold text-2xl  flex flex-col gap-10 items-center justify-center px-10 py-20  bg-white bg-opacity-20 ackdrop-blur-lg drop-shadow-lg rounded-3xl  sm:gap-20 sm:text-4xl sm:p-28">
+        <Link className="text-white bg-red rounded-3xl p-8 hover:bg-[#FBACB3]" href="/game/maps">
+          online game
         </Link>
-        <Link href="/game/ai-game">
-          <button className="text-white font-bold text-[35px] bg-red w-1/2 h-[90px] rounded-[20px] hover:bg-[#FBACB3]">
+        <Link className="text-white bg-red rounded-3xl p-8 hover:bg-[#FBACB3]" href="/game/ai-game">
             offline game
-          </button>
         </Link>
       </div>
     </main>
