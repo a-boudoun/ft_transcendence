40f83cdffbb0f1d360db86182d8af9e3b3c0804e
moelazhari <mazhari@student.1337.ma>
2023-10-02 16:40:47 +0100

    makeing maps page responsev

--- frontend/app/(login)/game/maps/page.tsx
+++ frontend/app/(login)/game/maps/page.tsx
@@ -22,24 +22,23 @@ export default function Maps() {
   }, [selectedMap]);
 
   return (
-    <main className="h-full bg-bg bg-cover w-full grid place-content-center pt-[56px]  ">
+    <main className="min-h-screen  grid place-content-center pt-14 bg-bg bg-cover">
       <div className="h-fit flex flex-col items-center gap-8 p-8 sm:gap-16  sm:bg-white sm:bg-opacity-20 sm:ackdrop-blur-lg sm:drop-shadow-lg sm:rounded-3xl">
-        <div className="bg-[#4b5468a0]  rounded-[10px] w-[300px] py-4">
-          <h3 className="text-[#B5B3BD] tracking-wide [word-spacing:3px] font-medium text-3xl">
-            {" "}
-            selet the map{" "}
+        <div className="bg-[#4b5468a0]  rounded-[10px] p-4 ">
+          <h3 className="text-[#B5B3BD] tracking-wide [word-spacing:3px] font-medium text-xl sm:text-3xl">
+            selet the map
           </h3>
         </div>
         <div className="flex justify-center gap-8 items-center flex-col sm:flex-row w-full">
           <button
-            className={`w-full flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE] rounded-b-[8px] ${
+            onClick={() => { setmapToLocalStorage("football-mode")}}
+            className={`flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE] rounded-b-[8px] ${
 				selectedMap === "football-mode"
-                ? "border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
+                ? "border-opacity-100 border-b-[#2DFCEE] rounded-b-[8px]"
                 : ""
             }`}
-			onClick={() => setmapToLocalStorage("football-mode")}
           >
-			<div className="max-w-[180px] sm:max-w-[260px]">
+			<div className="max-w-[260px]">
 				<Image
 				width={260}
 				height={260}
@@ -53,14 +52,14 @@ export default function Maps() {
             </p>
           </button>
           <button
-           className={`w-full flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE] rounded-b-[8px] ${
+           className={`flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE] rounded-b-[8px] ${
 			   selectedMap === "space-mode"
-			   ? "border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
+			   ? "border-opacity-100 border-b-[#2DFCEE] rounded-b-[8px]"
 			   : ""
 			}`}
 			onClick={() => setmapToLocalStorage("space-mode")}
           >
-			<div className="max-w-[180px] sm:max-w-[260px]">
+			<div className="max-w-[260px]">
 				<Image
 				width={260}
 				height={0}
@@ -74,14 +73,14 @@ export default function Maps() {
             </p>
           </button>
           <button
-          className={`w-full flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE]  rounded-b-[8px] ${
+          className={`flex flex-col items-center border-b-[5px] border-opacity-0  hover:border-b-[5px] hover:border-b-[#2DFCEE] hover:rounded-b-[8px] border-b-[#2DFCEE]  rounded-b-[8px] ${
 			selectedMap === "default"
-			? "border-b-[5px] border-b-[#2DFCEE] rounded-b-[8px]"
+			? "border-opacity-100 border-b-[#2DFCEE] rounded-b-[8px]"
 			: ""
 		 }`}
             onClick={() => setmapToLocalStorage("default")}
           >
-			<div className="max-w-[180px] sm:max-w-[260px]">
+			<div className="max-w-[260px]">
 				<Image
 				width={260}
 				height={0}
@@ -96,7 +95,7 @@ export default function Maps() {
           </button>
         </div>
         <button
-          className=" w-full max-w-[320px] sm:max-w-auto  bottom-[40px] right-[40px] text-white text-[30px] bg-red  py-2 rounded-[10px] hover:bg-[#FBACB3] font-bold"
+          className="text-white text-3xl bg-red  py-2 px-8 sm:px-16 rounded-[10px] hover:bg-[#FBACB3] font-bold"
           onClick={() => {
             router.push("/game/match");
           }}
